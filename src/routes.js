e8c319fc073d9af4dbb38efa812afafd9dacf21c
Kevin Alviola <kevin@istackmanila.com>
2017-08-04 18:26:51 +0800

    update react router with handling not found page

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/app';
-import Login from './modules/login'
-import DashBoard from './modules/dashboard'
-
+import Login from './modules/login';
+import DashBoard from './modules/dashboard';
+import NotFound from './modules/notFound';
 export default(
   <Route path="/" component={App}>
     <IndexRoute component={DashBoard}/>
     <Route path="login" component={Login}/>
     <Route path="dashboard" component={DashBoard}/>
+    <Route path="*" component={NotFound}/>
   </Route>
 );
