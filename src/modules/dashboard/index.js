d4d5760b951c794f6d7c5039ea2cc5cdedd6591e
Kevin Alviola <kevin@istackmanila.com>
2017-08-04 16:17:15 +0800

    update add routing

--- src/modules/dashboard/index.js
+++ src/modules/dashboard/index.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 
 class DashBoard extends Component {
-
+    render(){
+      return(
+        <div className="wrapper">
+          <h1>Welcome to DashBoard</h1>
+        </div>
+      );
+    }
 }
+
+export default DashBoard;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/app';
 import Login from './modules/login'
+import DashBoard from './modules/dashboard'
 
 export default(
-  <Route path="/login" component={Login}/>
+  <Route path="/" component={App}>
+    <IndexRoute component={DashBoard}/>
+    <Route path="login" component={Login}/>
+    <Route path="dashboard" component={DashBoard}/>
+  </Route>
 );
